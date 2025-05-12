b93949a33b55fac46a3206a46cb96489fcabd617
Per Vedin <per@vedin.pp.se>
2025-05-12 10:15:07 +0200

    messagebox (errors)

--- packages/vue/src/components/kontaktuppgifter/Kontaktuppgifter.vue
+++ packages/vue/src/components/kontaktuppgifter/Kontaktuppgifter.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { FTextField, FPhoneTextField, FEmailTextField } from "@fkui/vue";
+import { FTextField, FPhoneTextField, FEmailTextField, FFieldset, FTooltip, FMessageBox } from "@fkui/vue";
 import { type KontaktuppgifterType, useKontaktuppgifterType } from "./Kontaktuppgifter";
 
 interface Props {
@@ -14,6 +14,30 @@ const props = withDefaults(defineProps<Props>(), {
 
 <template>
 
+<f-fieldset>
+    <template #label>Kontaktuppgifter</template>
+    <template #description="{ descriptionClass }">
+        <span :class="descriptionClass">Fyll i kontaktuppgifter så att Bolagsverket vet vem dom ska kontakta i det här ärendet.</span>
+    </template>
+    <template #error-message>
+        <f-message-box type="error">
+            <template #default="{ headingSlotClass }">
+                <h3 :class="headingSlotClass">Rubrik</h3>
+                <p>Brödtext</p>
+            </template>
+        </f-message-box>
+    </template>
+    <template #tooltip>
+        <f-tooltip
+            screen-reader-text="Kontaktuppgifterna i ett ärende är obligatoriska uppgifter, dom används när Bolagsverket behöver kontakta dig i frågor rörande ditt ärende."
+            header-tag="h2"
+        >
+            <template #header>Läs mer om kontaktuppgifter</template>
+            <template #body>
+                Kontaktuppgifterna i ett ärende är obligatoriska uppgifter, dom används när Bolagsverket behöver kontakta dig i frågor rörande ditt ärende.
+            </template>
+        </f-tooltip>
+    </template>
     <f-text-field v-model="props.kontaktuppgifter.namn">
         <template #default>
             Förnamn och efternamn
@@ -37,5 +61,6 @@ const props = withDefaults(defineProps<Props>(), {
             Telefonnummer med landskod
         </template>
     </f-phone-text-field>
+</f-fieldset>
 
 </template>
