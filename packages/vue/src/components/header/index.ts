d8fd35406eafc58b1fa14cacecab3ae890f53d54
Per Vedin <per@vedin.pp.se>
2025-05-16 23:27:45 +0200

    fix(Delete header component): (fixes main)

--- packages/vue/src/components/header/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as BPageHeader } from "./BPageHeader.vue";
-export { type BPageHeaderType, useBPageHeaderType } from "./BPageHeaderType";
--- packages/vue/src/components/index.ts
+++ packages/vue/src/components/index.ts
@@ -2,4 +2,3 @@ export * from "./BExpandablePanels";
 export * from "./BForetagetsAdress";
 export * from "./BKontaktuppgifter";
 export * from "./BLayoutApplicationTemplate";
-export * from "./header";
